56243eacc5b3a399470f41f8298915a2f54ec09d
erolyagizaydin <32926423+ygzaydn@users.noreply.github.com>
2020-10-11 01:05:43 +0300

    signin

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,6 +1,6 @@
 import app from 'firebase/app'
 import 'firebase/auth'
-require('dotenv').config()
+
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,12 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const App = () => {
+import * as ROUTES from '../../constants/routes'
 
+const SignUpPage = () => {
     return (
-      <h1>
-        Sign Up
-      </h1>
+      <div>
+        <h1>Sign Up</h1>
+        <SignUpForm />
+      </div>
     )
 }
 
-export default App
+export const SignUpForm = () => {
+  const [username, setUsername ] = useState('')
+  const [email, setEmail ] = useState('')
+  const [passwordOne, setPasswordOne ] = useState('')
+  const [passwordTwo, setPasswordTwo ] = useState('')
+  const [error, setError ] = useState(null)
+
+
+  const onSubmit = (event) => {
+
+  }
+  const onChange = (setFunction) => (event) => {
+    setFunction(event.target.value)
+  }
+
+  const isInvalid = passwordOne !== passwordTwo || passwordOne === '' || email === '' || username === ''
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        name="username"
+        value={username}
+        onChange={onChange(setUsername)}
+        type="text"
+        placeholder="Full Name"
+      />
+      <input
+        name="email"
+        value={email}
+        onChange={onChange(setEmail)}
+        type="text"
+        placeholder="Email Adress"
+      />
+      <input
+        name="passwordOne"
+        value={passwordOne}
+        onChange={onChange(setPasswordOne)}
+        type="password"
+        placeholder="Password"
+      />
+      <input
+        name="passwordTwo"
+        value={passwordTwo}
+        onChange={onChange(setPasswordTwo)}
+        type="password"
+        placeholder="Confirm Password"
+      />
+      <button disabled={isInvalid} type="submit">Sign Up</button>
+
+      {error && <p>{error.message}</p>}
+    </form>
+  )
+}
+
+export const SignUpLink = () => (
+  <p>Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link></p>
+)
+
+export default SignUpPage
